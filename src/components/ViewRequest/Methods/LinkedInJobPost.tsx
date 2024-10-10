93d91038478f149782315d96db3070c621a25d1c
RobertPorterfield <57455266+RobertPorterfield@users.noreply.github.com>
2024-10-10 13:56:14 -0400

    move DCWF from LinkedIn to Details view

--- src/components/ViewRequest/Methods/LinkedInJobPost.tsx
+++ src/components/ViewRequest/Methods/LinkedInJobPost.tsx
@@ -1,8 +1,6 @@
 import { Label, Text } from "@fluentui/react-components";
 import { RPARequest } from "api/requestsApi";
-// import { DCWFCodes } from "consts/DCWF";
 import UpdatePostId from "components/ViewRequest/DetailSections/UpdatePostId";
-import { DCWFCodes } from "consts/DCWF";
 
 const ViewRequestLIJobPostDetails = (props: { data: RPARequest }) => {
   return (
@@ -42,56 +40,6 @@ const ViewRequestLIJobPostDetails = (props: { data: RPARequest }) => {
       <Text style={{ whiteSpace: "pre-wrap" }} id="linkedinQualifications">
         {props.data.linkedinQualifications?.map((qual) => qual + "\n")}
       </Text>
-
-      <fieldset style={{ gridColumn: "1 / 3" }}>
-        <legend>Defense Cyber Work Force (DCWF)</legend>
-        <table>
-          <thead>
-            <tr>
-              <th>Role</th>
-              <th>Proficiency</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                {props.data.dcwf?.length === 1
-                  ? props.data.dcwf +
-                      " " +
-                      DCWFCodes.find(
-                        ({ Code }) => Code === (props.data.dcwf[0] ?? "")
-                      )?.Role ?? ""
-                  : ""}
-              </td>
-              <td>{props.data.dcwfLevel}</td>
-            </tr>
-            <tr>
-              <td>
-                {props.data.dcwf2?.length === 1
-                  ? props.data.dcwf2 +
-                      " " +
-                      DCWFCodes.find(
-                        ({ Code }) => Code === (props.data.dcwf2[0] ?? "")
-                      )?.Role ?? ""
-                  : ""}
-              </td>
-              <td>{props.data.dcwf2Level}</td>
-            </tr>
-            <tr>
-              <td>
-                {props.data.dcwf3?.length === 1
-                  ? props.data.dcwf3 +
-                      " " +
-                      DCWFCodes.find(
-                        ({ Code }) => Code === (props.data.dcwf3[0] ?? "")
-                      )?.Role ?? ""
-                  : ""}
-              </td>
-              <td>{props.data.dcwf3Level}</td>
-            </tr>
-          </tbody>
-        </table>
-      </fieldset>
 
       <Label weight="semibold" htmlFor="linkedinKSAs">
         KSA's
